c40746f4fec9fc91428bca3132971064ca3a2403
Kaykay77777 <kay14019@gmail.com>
2025-02-23 22:36:37 +0900

    Update Python script

--- wine.py
+++ wine.py
@@ -390,8 +390,10 @@ def display_wine_cellar():
                     
                     if img_path:
                         st.write(f"画像URL: {img_path}")  # デバッグ用にURLを表示
+                        response = requests.get(img_path)
+                        img = Image.open(BytesIO(response.content))
                         #st.image(img_path, width=80, use_container_width=True)
-                        st.image(img_path, width=150)#, use_container_width=True)
+                        st.image(img, width=150)#, use_container_width=True)
                     else:
                         st.markdown('<div style="height:135px;"></div>', unsafe_allow_html=True)
 
