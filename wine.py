1fec74fc0fbdb8e0931abb9dd1ca66a1f7e8a103
Kaykay77777 <kay14019@gmail.com>
2025-02-23 18:33:14 +0900

    Update Python script

--- wine.py
+++ wine.py
@@ -387,7 +387,8 @@ def display_wine_cellar():
                     
                     if img_path:
                         st.write(f"画像URL: {img_path}")  # デバッグ用にURLを表示
-                        st.image(img_path, width=80, use_container_width=True)
+                        #st.image(img_path, width=80, use_container_width=True)
+                        st.image(img_path, width=80)#, use_container_width=True)
                     else:
                         st.markdown('<div style="height:135px;"></div>', unsafe_allow_html=True)
 
