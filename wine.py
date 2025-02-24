983d4328864e4299f37c501fb9ef8911cf85f507
Kaykay77777 <kay14019@gmail.com>
2025-02-24 18:51:00 +0900

    Update Python script

--- wine.py
+++ wine.py
@@ -615,7 +615,7 @@ if st.session_state.selected_location:
 
 st.subheader("markdown表示確認")
 st.markdown('<img src="https://drive.google.com/uc?id=104rzSGgccBQf8cAUvlL302XyU3j0W2k2" width="160"></iframe>', unsafe_allow_html=True)
-st.markdown('<img src="https://drive.google.com/uc?id=104rzSGgccBQf8cAUvlL302XyU3j0W2k2" width="160"/>')
+st.markdown('<img src="https://drive.google.com/uc?id=104rzSGgccBQf8cAUvlL302XyU3j0W2k" width="160"/>', unsafe_allow_html=True)
 
 st.markdown('<img src="https://drive.google.com/uc?id=104rzSGgccBQf8cAUvlL302XyU3j0W2k2" width="160" />', unsafe_allow_html=True)
 st.write('<img src="https://drive.google.com/uc?id=104rzSGgccBQf8cAUvlL302XyU3j0W2k2" width="160"/>', unsafe_allow_html=True)
